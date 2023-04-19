382d5fd20ffad391831e2560f59d0abccf76eaaf
010236 <010236@xx.com>
2023-04-19 12:15:24 +0800

    修改Swagger地址

--- src/views/infra/swagger/index.vue
+++ src/views/infra/swagger/index.vue
@@ -6,5 +6,5 @@
 <script setup lang="ts" name="Swagger">
 const BASE_URL = import.meta.env.VITE_BASE_URL
 // const src = ref(BASE_URL + '/doc.html')
-const src = ref(BASE_URL + '/swagger-ui')
+const src = ref(BASE_URL + '/doc.html')
 </script>
