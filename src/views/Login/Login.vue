27865313798f23f0fb4f74a15c04bf3c9c56c2c4
guorongsen <1021625624@qq.com>
2023-05-29 17:21:36 +0800

    fix: 代码更新

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,59 +1,18 @@
 <template>
   <div
     :class="prefixCls"
-    class="h-[100%] relative <xl:bg-v-dark <sm:px-10px <xl:px-10px <md:px-10px"
+    class="h-[100%] relative flex justify-center items-center <xl:bg-v-dark <sm:px-10px <xl:px-10px <md:px-10px"
   >
-    <div class="relative h-full flex mx-auto">
-      <div
-        :class="`${prefixCls}__left flex-1 bg-gray-500 bg-opacity-20 relative p-30px <xl:hidden`"
-      >
-        <!-- 左上角的 logo + 系统标题 -->
-        <div class="flex items-center relative text-white">
-          <!-- <img src="@/assets/imgs/logo.png" alt="" class="w-48px h-48px mr-10px" /> -->
-          <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>
-        </div>
-        <!-- 左边的背景图 + 欢迎语 -->
-        <div class="flex justify-center items-center h-[calc(100%-60px)]">
-          <TransitionGroup
-            appear
-            tag="div"
-            enter-active-class="animate__animated animate__bounceInLeft"
-          >
-            <img src="@/assets/svgs/login-box-bg.svg" key="1" alt="" class="w-350px" />
-            <div class="text-3xl text-white" key="2">{{ t('login.welcome') }}</div>
-            <!-- <div class="mt-5 font-normal text-white text-14px" key="3">
-              {{ t('login.message') }}
-            </div> -->
-          </TransitionGroup>
-        </div>
+    <div class="flex w-1100px h-712px">
+      <div :class="`login-left-block ${prefixCls}__left flex-1 pl-50px relative <xl:hidden`">
+        <div class="mt-70px mb-20px text-40px">Hello</div>
+        <div class="text-30px leading-tight">欢迎登录{{ underlineToHump(appStore.getTitle) }}</div>
       </div>
-      <div class="flex-1 p-30px <sm:p-10px dark:bg-v-dark relative">
-        <!-- 右上角的主题、语言选择 -->
-        <div class="flex justify-between items-center text-white @2xl:justify-end @xl:justify-end">
-          <div class="flex items-center @2xl:hidden @xl:hidden">
-            <img src="@/assets/imgs/logo.png" alt="" class="w-48px h-48px mr-10px" />
-            <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>
-          </div>
-          <div class="flex justify-end items-center space-x-10px">
-            <ThemeSwitch />
-            <LocaleDropdown class="<xl:text-white dark:text-white" />
-          </div>
-        </div>
-        <!-- 右边的登录界面 -->
-        <Transition appear enter-active-class="animate__animated animate__bounceInRight">
-          <div
-            class="h-full flex items-center m-auto w-[100%] @2xl:max-w-500px @xl:max-w-500px @md:max-w-500px @lg:max-w-500px"
-          >
-            <!-- 账号登录 -->
-            <LoginForm class="p-20px h-auto m-auto <xl:(rounded-3xl light:bg-white)" />
-            <!-- 手机登录 -->
-            <MobileForm class="p-20px h-auto m-auto <xl:(rounded-3xl light:bg-white)" />
-            <!-- 二维码登录 -->
-            <QrCodeForm class="p-20px h-auto m-auto <xl:(rounded-3xl light:bg-white)" />
-            <!-- 注册 -->
-            <RegisterForm class="p-20px h-auto m-auto <xl:(rounded-3xl light:bg-white)" />
-          </div>
-        </Transition>
+      <div
+        class="login-right-block h-full flex m-auto w-[100%] @2xl:max-w-460px @xl:max-w-460px @md:max-w-460px @lg:max-w-460px"
+      >
+        <!-- 账号登录 -->
+        <NewLoginForm />
       </div>
     </div>
   </div>
@@ -63,11 +22,8 @@ import { underlineToHump } from '@/utils'
 
 import { useDesign } from '@/hooks/web/useDesign'
 import { useAppStore } from '@/store/modules/app'
-import { ThemeSwitch } from '@/layout/components/ThemeSwitch'
-import { LocaleDropdown } from '@/layout/components/LocaleDropdown'
-import { LoginForm, MobileForm, RegisterForm, QrCodeForm } from './components'
+import { NewLoginForm } from './components'
 
-const { t } = useI18n()
 const appStore = useAppStore()
 const { getPrefixCls } = useDesign()
 const prefixCls = getPrefixCls('login')
@@ -77,6 +33,8 @@ const prefixCls = getPrefixCls('login')
 $prefix-cls: #{$namespace}-login;
 
 .#{$prefix-cls} {
+  background: url('@/assets/imgs/login-bg.png') no-repeat center;
+  background-size: cover;
   &__left {
     &::before {
       position: absolute;
@@ -90,6 +48,15 @@ $prefix-cls: #{$namespace}-login;
       background-repeat: no-repeat;
       content: '';
     }
+  }
+  .login-left-block {
+    color: #ffffff;
+    background: url('@/assets/imgs/login-block.png') no-repeat center;
+    background-size: contain;
+  }
+  .login-right-block {
+    background-color: #ffffff;
+    box-shadow: 6px 5px 16px 0px rgba(212, 212, 212, 0.5);
   }
 }
 </style>
