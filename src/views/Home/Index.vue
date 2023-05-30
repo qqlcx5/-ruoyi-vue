41e03252319131a6053a466809d04e1a6d2928bd
guorongsen <1021625624@qq.com>
2023-05-30 15:27:26 +0800

    fix: 首页缺省页

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col justify-center items-center w-full h-full bg-white">
+  <div class="flex flex-col justify-center items-center w-full h-full content-bg">
     <div class="home-content w-full h-[60%]"></div>
-    <div class="h-60px text-30px mt-30px text-title">欢迎使用万国投资管理系统</div>
+    <div class="h-60px text-30px mt-30px">欢迎使用万国投资管理系统</div>
   </div>
 </template>
 <script setup lang="ts" name="Home"></script>
