5de329d458e8dc8beb73d4dfc68a4033a37edc68
guorongsen <1021625624@qq.com>
2023-06-06 11:09:21 +0800

    fix: bug修复

--- src/views/system/role/component/ConfigContent.vue
+++ src/views/system/role/component/ConfigContent.vue
@@ -101,7 +101,7 @@
         </div>
       </div>
       <div>
-        <div class="box-header"> 请选择数据权限 </div>
+        <div class="box-header"> 请选择品牌权限 </div>
         <div class="box-content px-14px py-10px">
           <div v-if="currentNode && currentNode.type === 2">
             <el-radio-group
@@ -110,7 +110,7 @@
               @change="onBrandScopeChange"
             >
               <el-radio :label="1">不限品牌</el-radio>
-              <el-radio :label="2">指定品牌</el-radio>
+              <el-radio :label="2"> 指定品牌 </el-radio>
               <el-checkbox
                 v-if="currentNode.databrandScope === 2"
                 class="!absolute right-0 bottom-0"
@@ -121,6 +121,9 @@
               </el-checkbox>
             </el-radio-group>
             <div v-show="currentNode.databrandScope === 2" class="ml-24px">
+              <span v-if="selectedBrands.length === 0" class="text-error -ml-24px"
+                >(请至少选择一个指定品牌)</span
+              >
               <el-checkbox-group v-model="selectedBrands">
                 <el-checkbox v-for="item in dictBrand" :key="item.value" :label="item.value">{{
                   item.label
@@ -150,6 +153,7 @@ import { getDictOptions } from '@/utils/dict'
 import { getTenantData } from '@/utils/auth'
 
 const { query } = useRoute()
+const message = useMessage()
 
 const props = defineProps({
   stage: {
@@ -349,6 +353,10 @@ watch(
 const getParams = () => {
   const menuDataScopeItemList = cloneDeep(treeRef.value!.getCheckedNodes(false, true))
   return menuDataScopeItemList.map((item) => {
+    if (item.databrandScope === 2 && item.dataScopeBrandIds.length === 0) {
+      message.error(`${item.name}中未选择指定品牌`)
+      throw new Error(`${item.name}中未选择指定品牌`)
+    }
     item['menuId'] = item.id
     item['menuParentId'] = item.parentId
     delete item.children
