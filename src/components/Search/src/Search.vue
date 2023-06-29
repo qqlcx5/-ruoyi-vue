c20b96d58b020e728bd94f6bd4f1dad1ebbb800c
pansr <870054050@qq.com>
2023-06-29 19:15:17 +0800

    feat: 表格查询默认开启展开模式，超出一行显示展开

--- src/components/Search/src/Search.vue
+++ src/components/Search/src/Search.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { PropType } from 'vue'
+import { PropType, onMounted, computed } from 'vue'
 import { propTypes } from '@/utils/propTypes'
 
 import { useForm } from '@/hooks/web/useForm'
@@ -30,7 +30,7 @@ const props = defineProps({
   showSearch: propTypes.bool.def(true),
   showReset: propTypes.bool.def(true),
   // 是否显示伸缩
-  expand: propTypes.bool.def(false),
+  expand: propTypes.bool.def(true),
   // 伸缩的界限字段
   expandField: propTypes.string.def(''),
   inline: propTypes.bool.def(true),
@@ -42,12 +42,58 @@ const props = defineProps({
 
 const emit = defineEmits(['search', 'reset'])
 
-const visible = ref(true)
+const visible = ref(false)
+const formBoxRef = ref()
+const defaultExpanField = ref('')
+
+const getExpanField = computed(() => props.expandField || defaultExpanField.value)
+const showExpandBtn = computed(() => getExpanField.value && props.expand)
+
+onMounted(() => {
+  setDefaultShowOneLint()
+})
+
+/** 设置默认值显示一行，其他收起 */
+const setDefaultShowOneLint = () => {
+  const formItems = formBoxRef.value.querySelector('form')?.querySelectorAll('.el-form-item')
+  const form = formBoxRef.value.querySelector('form')
+  const lastFormItemIndex = formItems.length - 1
+  const lastFormItemWidth =
+    formItems[lastFormItemIndex].offsetWidth +
+    Number(window.getComputedStyle(formItems[lastFormItemIndex]).marginRight?.replaceAll('px', ''))
+  let itemsWidth = 0
+  const getItemWidth = (item) => {
+    const styles = window.getComputedStyle(item).marginRight
+    const itemMargin = Number(styles.replaceAll('px', '')) || 0
+    const itemWidth = item.offsetWidth
+    return itemMargin + itemWidth
+  }
+  for (let i = 0; i < formItems.length; i++) {
+    const item = formItems[i]
+    const index = i
+    const formWidth = form.offsetWidth
+    const itemWidth = getItemWidth(item)
+    itemsWidth += itemWidth
+    if (lastFormItemIndex !== index) {
+      // 判断一行是否还能放下下一个查询组件
+      if (formWidth - itemsWidth < getItemWidth(formItems[index + 1]) + 5) {
+        // 判断一行是否还能放下查询重置按钮，不能就把当前及之后的选项收起
+        if (formWidth - itemsWidth < lastFormItemWidth + 90) {
+          defaultExpanField.value = props.schema[index - 1].field
+          break
+        }
+        defaultExpanField.value = props.schema[index].field
+        break
+      }
+    }
+  }
+}
 
 const newSchema = computed(() => {
   let schema: FormSchema[] = cloneDeep(props.schema)
-  if (props.expand && props.expandField && !unref(visible)) {
-    const index = findIndex(schema, (v: FormSchema) => v.field === props.expandField)
+
+  if (props.expand && getExpanField.value && !unref(visible)) {
+    const index = findIndex(schema, (v: FormSchema) => v.field === getExpanField.value)
     if (index > -1) {
       const length = schema.length
       schema.splice(index + 1, length)
@@ -63,6 +109,7 @@ const newSchema = computed(() => {
       }
     ])
   }
+
   return schema
 })
 
@@ -100,7 +147,7 @@ const setVisible = () => {
 </script>
 
 <template>
-  <div>
+  <div ref="formBoxRef">
     <Form
       :is-custom="false"
       :label-width="labelWidth"
@@ -118,7 +165,7 @@ const setVisible = () => {
           <ElButton v-if="showReset" @click="reset">
             {{ t('common.reset') }}
           </ElButton>
-          <ElButton v-if="expand" text @click="setVisible">
+          <ElButton v-if="showExpandBtn" text @click="setVisible">
             {{ t(visible ? 'common.shrink' : 'common.expand') }}
             <Icon :icon="visible ? 'ep:arrow-up' : 'ep:arrow-down'" />
           </ElButton>
@@ -139,7 +186,7 @@ const setVisible = () => {
           <Icon icon="ep:refresh-right" class="mr-5px" />
           {{ t('common.reset') }}
         </ElButton>
-        <ElButton v-if="expand" text @click="setVisible">
+        <ElButton v-if="showExpandBtn" text @click="setVisible">
           {{ t(visible ? 'common.shrink' : 'common.expand') }}
           <Icon :icon="visible ? 'ep:arrow-up' : 'ep:arrow-down'" />
         </ElButton>
