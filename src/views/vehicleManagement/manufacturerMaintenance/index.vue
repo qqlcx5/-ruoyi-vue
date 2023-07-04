524c9872164b46f422a0ce871298ac7e8d3f3ff8
lcx <qqlcx5@163.com>
2023-07-04 16:49:18 +0800

    feat: 整车管理

---
+++ src/views/vehicleManagement/manufacturerMaintenance/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 厂家维护 manufacturerMaintenance </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/views/vehicleManagement/vehicleData/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 整车数据 整车数据 </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
